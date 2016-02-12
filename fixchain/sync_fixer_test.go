e41928dd4f1d64256f824e243f318b88f2f2abb1
Kat Joyce <katjoyce@google.com>
2016-02-12 16:25:17 +0000

    Fixer tests

---
+++ fixchain/sync_fixer_test.go
@@ -0,0 +1,53 @@
+package fixchain
+
+import (
+	"net/http"
+	"testing"
+)
+
+// Fixer.Fix() tests
+func TestSyncFixNone(t *testing.T) {
+	f := NewFixer(&http.Client{})
+	chains, ferrs := f.Fix(nil)
+	if chains != nil {
+		t.Errorf("Chains produced")
+	}
+	if ferrs != nil {
+		t.Errorf("Errors produced")
+	}
+}
+
+func TestSyncFixSingle(t *testing.T) {
+	f := NewFixer(&http.Client{})
+	for i, test := range handleChainTests {
+		chainsToFix := []*ChainToFix{
+			&ChainToFix{
+				cert:  GetTestCertificateFromPEM(t, test.cert),
+				chain: extractTestChain(t, i, test.chain),
+				roots: extractTestRoots(t, i, test.roots),
+			},
+		}
+		chains, ferrs := f.Fix(chainsToFix)
+		matchTestChainList(t, i, test.expectedChains, chains)
+		matchTestErrorList(t, i, test.expectedErrs, ferrs)
+	}
+}
+
+func TestSyncFixMultiple(t *testing.T) {
+	f := NewFixer(&http.Client{})
+	var chainsToFix []*ChainToFix
+	var expectedChains [][]string
+	var expectedErrs []errorType
+	for _, test := range handleChainTests {
+		chainsToFix = append(chainsToFix, &ChainToFix{
+			cert:  GetTestCertificateFromPEM(t, test.cert),
+			chain: extractTestChain(t, 0, test.chain),
+			roots: extractTestRoots(t, 0, test.roots),
+		})
+		expectedChains = append(expectedChains, test.expectedChains...)
+		expectedErrs = append(expectedErrs, test.expectedErrs...)
+	}
+	chains, ferrs := f.Fix(chainsToFix)
+	matchTestChainList(t, 0, expectedChains, chains)
+	matchTestErrorList(t, 0, expectedErrs, ferrs)
+}
