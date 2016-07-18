b2bf99388b7956d4a6854ba2b20084657bf4b0ea
Pierre Phaneuf <pphaneuf@google.com>
2016-07-18 15:14:34 +0100

    Let the json module decode base64 for us.

--- client/getentries.go
+++ client/getentries.go
@@ -2,22 +2,21 @@ package client
 
 import (
 	"bytes"
-	"encoding/base64"
 	"errors"
 	"fmt"
 
 	ct "github.com/google/certificate-transparency/go"
 )
 
-// base64LeafEntry respresents a Base64 encoded leaf entry
-type base64LeafEntry struct {
-	LeafInput string `json:"leaf_input"`
-	ExtraData string `json:"extra_data"`
+// leafEntry respresents a JSON leaf entry
+type leafEntry struct {
+	LeafInput []byte `json:"leaf_input"`
+	ExtraData []byte `json:"extra_data"`
 }
 
 // getEntriesReponse respresents the JSON response to the CT get-entries method
 type getEntriesResponse struct {
-	Entries []base64LeafEntry `json:"entries"` // the list of returned entries
+	Entries []leafEntry `json:"entries"` // the list of returned entries
 }
 
 // GetEntries attempts to retrieve the entries in the sequence [|start|, |end|] from the CT log server. (see section 4.6.)
@@ -36,21 +35,19 @@ func (c *LogClient) GetEntries(start, end int64) ([]ct.LogEntry, error) {
 	}
 	entries := make([]ct.LogEntry, len(resp.Entries))
 	for index, entry := range resp.Entries {
-		leafBytes, err := base64.StdEncoding.DecodeString(entry.LeafInput)
-		leaf, err := ct.ReadMerkleTreeLeaf(bytes.NewBuffer(leafBytes))
+		leaf, err := ct.ReadMerkleTreeLeaf(bytes.NewBuffer(entry.LeafInput))
 		if err != nil {
 			return nil, err
 		}
 		entries[index].Leaf = *leaf
-		chainBytes, err := base64.StdEncoding.DecodeString(entry.ExtraData)
 
 		var chain []ct.ASN1Cert
 		switch leaf.TimestampedEntry.EntryType {
 		case ct.X509LogEntryType:
-			chain, err = ct.UnmarshalX509ChainArray(chainBytes)
+			chain, err = ct.UnmarshalX509ChainArray(entry.ExtraData)
 
 		case ct.PrecertLogEntryType:
-			chain, err = ct.UnmarshalPrecertChainArray(chainBytes)
+			chain, err = ct.UnmarshalPrecertChainArray(entry.ExtraData)
 
 		default:
 			return nil, fmt.Errorf("saw unknown entry type: %v", leaf.TimestampedEntry.EntryType)
