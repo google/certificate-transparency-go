8e13e193463f467e8f5d0f4d74f842fedda0ddd3
David Drysdale <drysdale@google.com>
2016-11-29 20:20:57 +0000

    scanner: add option to dump matched certs

--- scanner/scanlog/scanlog.go
+++ scanner/scanlog/scanlog.go
@@ -19,9 +19,11 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"net/http"
+	"path"
 	"regexp"
 	"time"
 
@@ -50,18 +52,70 @@ var parallelFetch = flag.Int("parallel_fetch", 2, "Number of concurrent GetEntri
 var startIndex = flag.Int64("start_index", 0, "Log index to start scanning at")
 var quiet = flag.Bool("quiet", false, "Don't print out extra logging messages, only matches.")
 var printChains = flag.Bool("print_chains", false, "If true prints the whole chain rather than a summary")
+var dumpDir = flag.String("dump_dir", "", "Directory to store matched certificates in")
+
+func dumpData(entry *ct.LogEntry) {
+	if *dumpDir == "" {
+		return
+	}
+	chainFrom := 0
+	prefix := "unknown"
+	if entry.Leaf.TimestampedEntry.EntryType == ct.X509LogEntryType {
+		prefix = "cert"
+		name := fmt.Sprintf("%s-%014d-leaf.der", prefix, entry.Index)
+		filename := path.Join(*dumpDir, name)
+		err := ioutil.WriteFile(filename, entry.Leaf.TimestampedEntry.X509Entry.Data, 0644)
+		if err != nil {
+			log.Printf("Failed to dump data for %s at index %d: %v", prefix, entry.Index, err)
+		}
+	} else if entry.Leaf.TimestampedEntry.EntryType == ct.PrecertLogEntryType {
+		prefix = "pecert"
+		// For a pre-certificate the TimestampedEntry only holds the TBSCertificate, but
+		// the Chain data has the full pre-certificate as the first entry.
+		name := fmt.Sprintf("%s-%014d-precert.der", prefix, entry.Index)
+		filename := path.Join(*dumpDir, name)
+		if len(entry.Chain) == 0 {
+			log.Printf("Precert entry missing chain[0] at index %d", entry.Index)
+			return
+		}
+		if err := ioutil.WriteFile(filename, entry.Chain[0].Data, 0644); err != nil {
+			log.Printf("Failed to dump data for %s at index %d: %v", prefix, entry.Index, err)
+		}
+		chainFrom = 1
+	} else {
+		log.Printf("Unknown log entry type %d", entry.Leaf.TimestampedEntry.EntryType)
+	}
+	for ii := chainFrom; ii < len(entry.Chain); ii++ {
+		name := fmt.Sprintf("%s-%014d-%02d.der", prefix, entry.Index, ii)
+		filename := path.Join(*dumpDir, name)
+		err := ioutil.WriteFile(filename, entry.Chain[ii].Data, 0644)
+		if err != nil {
+			log.Printf("Failed to dump data for CA at index %d: %v", entry.Index, err)
+		}
+	}
+}
 
 // Prints out a short bit of info about |cert|, found at |index| in the
 // specified log
 func logCertInfo(entry *ct.LogEntry) {
-	log.Printf("Interesting cert at index %d: CN: '%s'", entry.Index, entry.X509Cert.Subject.CommonName)
+	if entry.X509Cert != nil {
+		log.Printf("Process cert at index %d: CN: '%s'", entry.Index, entry.X509Cert.Subject.CommonName)
+		dumpData(entry)
+	} else {
+		log.Printf("Process cert at index %d: <unparsed>", entry.Index)
+	}
 }
 
 // Prints out a short bit of info about |precert|, found at |index| in the
 // specified log
 func logPrecertInfo(entry *ct.LogEntry) {
-	log.Printf("Interesting precert at index %d: CN: '%s' Issuer: %s", entry.Index,
-		entry.Precert.TBSCertificate.Subject.CommonName, entry.Precert.TBSCertificate.Issuer.CommonName)
+	if entry.Precert != nil {
+		log.Printf("Process precert at index %d: CN: '%s' Issuer: %s", entry.Index,
+			entry.Precert.TBSCertificate.Subject.CommonName, entry.Precert.TBSCertificate.Issuer.CommonName)
+		dumpData(entry)
+	} else {
+		log.Printf("Process precert at index %d: <unparsed>", entry.Index)
+	}
 }
 
 func chainToString(certs []ct.ASN1Cert) string {
