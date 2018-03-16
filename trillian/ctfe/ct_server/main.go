0119e9dd827c78fc9c58c4c9f51221e01f389a24
Al Cutter <al@google.com>
2018-03-16 16:56:16 +0000

    fixes

--- trillian/ctfe/ct_server/main.go
+++ trillian/ctfe/ct_server/main.go
@@ -158,8 +158,6 @@ func main() {
 		go func() {
 			mux := http.NewServeMux()
 			mux.Handle("/metrics", promhttp.Handler())
-			// Return a 200 on the root, for GCE default health checking :/
-			mux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) { resp.WriteHeader(200) })
 			metricsServer := http.Server{Addr: metricsAt, Handler: mux}
 			err := metricsServer.ListenAndServe()
 			glog.Warningf("Metrics server exited: %v", err)
@@ -168,6 +166,9 @@ func main() {
 		// Handle metrics on the DefaultServeMux.
 		http.Handle("/metrics", promhttp.Handler())
 	}
+
+	// Return a 200 on the root, for GCE default health checking :/
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) { resp.WriteHeader(http.StatusOK) })
 
 	if *getSTHInterval > 0 {
 		// Regularly update the internal STH for each log so our metrics stay up-to-date with any tree head
