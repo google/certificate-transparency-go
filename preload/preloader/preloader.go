070623830cb9eb2a0aa29ecca72df8c483d1bd8e
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2018-04-24 12:19:02 -0700

    Use the right chain when logging

--- preload/preloader/preloader.go
+++ preload/preloader/preloader.go
@@ -114,10 +114,10 @@ func precertSubmitter(ctx context.Context, addedCerts chan<- *preload.AddedCert,
 		sct, err := logClient.AddPreChain(ctx, chain)
 		if err != nil {
 			log.Printf("failed to add pre-chain with CN %s: %v", c.Precert.TBSCertificate.Subject.CommonName, err)
-			recordFailure(addedCerts, c.Chain[0], err)
+			recordFailure(addedCerts, chain[0], err)
 			continue
 		}
-		recordSct(addedCerts, c.Chain[0], sct)
+		recordSct(addedCerts, chain[0], sct)
 		if !*quiet {
 			log.Printf("Added precert chain for CN '%s', SCT: %s\n", c.Precert.TBSCertificate.Subject.CommonName, sct)
 		}
