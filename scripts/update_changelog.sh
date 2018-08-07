e0424b72b98a68c457266c830b3bd51e3d7f2b5d
Rob Percival <robpercival@google.com>
2018-08-07 14:10:03 +0100

    Format update_changelog.sh

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -1,10 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 #
 # Run this script from the top level directory of the ct-go repo e.g.
 # with scripts/update_changelog.sh.
 
-# Get and build the correct branch that includes markdown output
-# TODO(Martin2112): replace with upstream repo if/when aktau/github-release#81 is merged
+# Get and build the correct fork that includes markdown output.
+# TODO(Martin2112): replace with upstream repo if/when aktau/github-release#81
+# is merged.
 go get -u github.com/Martin2112/github-release
 
 # Generate the changelog.
