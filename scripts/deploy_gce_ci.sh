3938234e24eda311c22375b747a134a6013ea427
Rob Percival <robpercival@google.com>
2018-11-16 17:58:15 +0000

    Consistently use $PROJECT_ID instead of $PROJECT_NAME
    The trillian/examples/deployment/scripts/deploy.sh script uses $PROJECT_ID,
    and Google Cloud Build also uses $PROJECT_ID. Right now, CI pushes via
    Travis are (silently) failing because of this inconsistency.

--- scripts/deploy_gce_ci.sh
+++ scripts/deploy_gce_ci.sh
@@ -6,7 +6,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export PROJECT_NAME=trillian-opensource-ci
+export PROJECT_ID=trillian-opensource-ci
 export CLUSTER_NAME=trillian-opensource-ci
 export MASTER_ZONE=us-central1-a
 
