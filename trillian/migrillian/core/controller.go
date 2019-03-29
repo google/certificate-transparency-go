20fc7f5767ee0b8aec7e596a8fee8ad6838a4a67
Pavel Kalinnikov <pkalinnikov@google.com>
2019-03-29 13:56:27 +0000

    Add TODO

--- trillian/migrillian/core/controller.go
+++ trillian/migrillian/core/controller.go
@@ -272,6 +272,7 @@ func (c *Controller) fetchTail(ctx context.Context, begin uint64) (uint64, error
 	if fo.Continuous { // Ignore range parameters in continuous mode.
 		fo.StartIndex, fo.EndIndex = int64(root.TreeSize), 0
 		// Use non-continuous Fetcher, as we implement continuity in Controller.
+		// TODO(pavelkalinnikov): Don't overload Fetcher's Continuous flag.
 		fo.Continuous = false
 	} else if fo.StartIndex < 0 {
 		fo.StartIndex = int64(root.TreeSize)
