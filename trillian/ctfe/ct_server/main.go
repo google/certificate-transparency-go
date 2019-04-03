80549a60e3c147b7ee5f0cbf3c09eb41a117cc74
David Drysdale <drysdale@google.com>
2019-04-03 16:04:48 +0100

    Temporary workaround for upstream cancellation problem

--- trillian/ctfe/ct_server/main.go
+++ trillian/ctfe/ct_server/main.go
@@ -112,7 +112,12 @@ func main() {
 	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
 	if len(*etcdServers) > 0 {
 		// Use etcd to provide endpoint resolution.
-		cfg := clientv3.Config{Endpoints: strings.Split(*etcdServers, ","), DialTimeout: 5 * time.Second}
+		// TODO(daviddrysdale): re-enable dial timeout when upstream client code fixed
+		// for https://github.com/grpc/grpc-go/pull/2733/files#r271705181
+		cfg := clientv3.Config{
+			Endpoints: strings.Split(*etcdServers, ","),
+			// DialTimeout: 5 * time.Second,
+		}
 		client, err := clientv3.New(cfg)
 		if err != nil {
 			glog.Exitf("Failed to connect to etcd at %v: %v", *etcdServers, err)
