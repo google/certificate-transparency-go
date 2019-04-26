e4670a0113479942be3a67c728088c1ce9ecd685
Tatiana Merkulova <merkulova@google.com>
2019-04-26 14:59:09 +0100

    Adding package/func comments (#516)
    * Adding package/func comments

--- submission/proxy.go
+++ submission/proxy.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package submission contains code and structs for certificates submission proxy.
 package submission
 
 import (
--- submission/server/main.go
+++ submission/server/main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// The submission_server runs (pre-)certs multi-Log submission complying with
+// CT-policy provided.
 package main
 
 import (
