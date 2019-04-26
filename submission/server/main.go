8ff37c37c38ee9ec95c2b9c3060204e862405077
Mercurrent <merkulova@google.com>
2019-04-26 14:41:34 +0100

    Adding package/func comments

--- submission/server/main.go
+++ submission/server/main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Submission proxy server runs (pre-)certs multi-Log submission complying with
+// CT-policy provided.
 package main
 
 import (
