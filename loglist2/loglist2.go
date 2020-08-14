b45213476483eb1ba7d4c41628606f2d91f7aef2
marc <marc.berhault@gmail.com>
2020-08-14 18:16:39 +0200

    loglist2: update URLs to v2 (from v2_beta)
    The LogList/LogListSignature/AllLogList URLs were still from v2_beta
    which is no longer available. Update to v2 URLs.
    
    Also mention `loglist2` in the main repo README and tweak `loglist`
    description.

--- loglist2/loglist2.go
+++ loglist2/loglist2.go
@@ -36,11 +36,11 @@ import (
 
 const (
 	// LogListURL has the master URL for Google Chrome's log list.
-	LogListURL = "https://www.gstatic.com/ct/log_list/v2beta/log_list.json"
+	LogListURL = "https://www.gstatic.com/ct/log_list/v2/log_list.json"
 	// LogListSignatureURL has the URL for the signature over Google Chrome's log list.
-	LogListSignatureURL = "https://www.gstatic.com/ct/log_list/v2beta/log_list.sig"
+	LogListSignatureURL = "https://www.gstatic.com/ct/log_list/v2/log_list.sig"
 	// AllLogListURL has the URL for the list of all known logs (which isn't signed).
-	AllLogListURL = "https://www.gstatic.com/ct/log_list/v2beta/all_logs_list.json"
+	AllLogListURL = "https://www.gstatic.com/ct/log_list/v2/all_logs_list.json"
 )
 
 // Manually mapped from https://www.gstatic.com/ct/log_list/v2beta/log_list_schema.json
