a52c5d21aacfd70e7e98e9409a93620d8d547ff6
Martin Hutchinson <mhutchinson@gmail.com>
2022-05-26 09:56:24 +0100

    Moved tools.go to its own directory to be consistent with other repos
    As reported in #936 this should help with vendored directories too.

--- tools/tools.go
+++ tools/tools.go
@@ -15,6 +15,8 @@
 //go:build tools
 // +build tools
 
+// Package tools tracks dependencies on binaries not otherwise referenced in this codebase.
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 package tools
 
 import (
