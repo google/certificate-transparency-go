a66768105cb9cd6d654477a716655f8868440a57
Martin Hutchinson <mhutchinson@gmail.com>
2022-06-17 16:18:10 +0100

    Removed references to etcdiscover tool
    This was removed in trillian#2376. If this has value to anyone then it would be great to take a pull request to add this back. In the meantime, this at least removes dead references.

--- trillian/integration/ct_functions.sh
+++ trillian/integration/ct_functions.sh
@@ -19,7 +19,6 @@ readonly CT_GO_PATH=$(go list -f '{{.Dir}}' github.com/google/certificate-transp
 #  - CT_SERVER_PIDS     : bash array of CT HTTP server pids
 # in addition to the variables populated by log_prep_test.
 # If etcd and Prometheus are configured, it also populates:
-#  - ETCDISCOVER_PID   : pid of etcd service watcher
 #  - PROMETHEUS_PID    : pid of local Prometheus server
 #  - PROMETHEUS_CFGDIR : Prometheus configuration directory
 ct_prep_test() {
@@ -63,17 +62,9 @@ ct_prep_test() {
     if [[ ! -z "${ETCD_OPTS}" ]]; then
         PROMETHEUS_CFGDIR="$(mktemp -d ${TMPDIR}/ct-prometheus-XXXXXX)"
         local prom_cfg="${PROMETHEUS_CFGDIR}/config.yaml"
-        local etcdiscovered="${PROMETHEUS_CFGDIR}/trillian.json"
-        sed "s!@ETCDISCOVERED@!${etcdiscovered}!" ${CT_GO_PATH}/trillian/integration/prometheus.yml > "${prom_cfg}"
         echo "Prometheus configuration in ${prom_cfg}:"
         cat ${prom_cfg}
 
-        echo "Building etcdiscover"
-        go build github.com/google/trillian/monitoring/prometheus/etcdiscover
-
-        echo "Launching etcd service monitor updating ${etcdiscovered}"
-        ./etcdiscover ${ETCD_OPTS} --etcd_services=trillian-ctfe-metrics-http,trillian-logserver-http,trillian-logsigner-http -target=${etcdiscovered} --logtostderr &
-        ETCDISCOVER_PID=$!
         echo "Launching Prometheus (default location localhost:9090)"
         ${PROMETHEUS_DIR}/prometheus --config.file=${prom_cfg} \
                            --web.console.templates=${CT_GO_PATH}/trillian/integration/consoles \
@@ -143,9 +134,6 @@ ct_stop_test() {
   local pids
   if [[ "${PROMETHEUS_PID}" != "" ]]; then
     pids+=" ${PROMETHEUS_PID}"
-  fi
-  if [[ "${ETCDISCOVER_PID}" != "" ]]; then
-    pids+=" ${ETCDISCOVER_PID}"
   fi
   echo "Stopping CT HTTP servers (pids ${CT_SERVER_PIDS[@]})"
   pids+=" ${CT_SERVER_PIDS[@]}"
