4b570af72c7596736faeface3feecb37242bd418
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-08 02:48:47 -0400

    spelling: issuers
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- trillian/ctfe/instance.go
+++ trillian/ctfe/instance.go
@@ -61,7 +61,7 @@ type InstanceOptions struct {
 	// CertificateQuotaUser returns a string represeing the passed in
 	// intermediate certificate. This string will be user as a User quota key for
 	// the cert. Quota will be requested for each intermediate in an
-	// add-[pre]-chain request so as to allow individual issers to be rate
+	// add-[pre]-chain request so as to allow individual issuers to be rate
 	// limited. If unset, no quota will be requested for intermediate
 	// certificates.
 	CertificateQuotaUser func(*x509.Certificate) string
