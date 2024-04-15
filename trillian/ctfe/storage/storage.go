291addb9e100127b0ded1a95253233cbbaa35706
Roger Ng <rogerng@google.com>
2024-04-15 15:34:43 +0000

    Add non-leaf certificate chain storage interface

---
+++ trillian/ctfe/storage/storage.go
@@ -0,0 +1,17 @@
+package storage
+
+import (
+	"context"
+)
+
+// NonLeafCertificateChainStorage is an interface which allows CTFE binaries to use different storage implementations for non-leaf certificate chains.
+type NonLeafCertificateChainStorage interface {
+	// FindAll returns all key-value pairs of non-leaf certificate chains.
+	FindAll(ctx context.Context) (map[string][]byte, error)
+
+	// FindByHash returns the non-leaf certificate chain by the hash.
+	FindByHash(ctx context.Context, hash string) ([]byte, error)
+
+	// Add inserts the key-value pair of non-leaf certificate chain.
+	Add(ctx context.Context, hash string, chain []byte) error
+}
