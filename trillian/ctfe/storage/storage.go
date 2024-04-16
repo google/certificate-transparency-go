353d91bde08e8e366bc184ee2b640fd615947b19
Roger Ng <rogerng@google.com>
2024-04-16 21:44:08 +0000

    Update`NonLeafCertificateChainStorage.FindByHash` comment

--- trillian/ctfe/storage/storage.go
+++ trillian/ctfe/storage/storage.go
@@ -9,7 +9,7 @@ type NonLeafCertificateChainStorage interface {
 	// FindAll returns all key-value pairs of non-leaf certificate chains.
 	FindAll(ctx context.Context) (map[string][]byte, error)
 
-	// FindByHash returns the non-leaf certificate chain by the hash.
+	// FindByHash returns the non-leaf certificate chain associated with the provided hash.
 	FindByHash(ctx context.Context, hash string) ([]byte, error)
 
 	// Add inserts the key-value pair of non-leaf certificate chain.
