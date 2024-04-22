c80d10bb6dbe82343434de3367009a6df365c17e
Roger Ng <rogerng@google.com>
2024-04-22 16:27:18 +0000

    Rename `NonLeafCertificateChain` to `IssuanceChain`

--- trillian/ctfe/cache/cache.go
+++ trillian/ctfe/cache/cache.go
@@ -16,18 +16,18 @@ package cache
 
 import "context"
 
-// NonLeafCertificateChainCache is an interface which allows CTFE binaries to use different cache implementations for non-leaf certificate chains.
-type NonLeafCertificateChainCache interface {
+// IssuanceChainCache is an interface which allows CTFE binaries to use different cache implementations for issuance chains.
+type IssuanceChainCache interface {
 	// IsLazyLoading returns whether lazy loading is enabled.
 	IsLazyLoading() bool
 
 	// Contains returns whether the hash exists in the cache.
 	Contains(ctx context.Context, hash string) (bool, error)
 
-	// Get returns the non-leaf certificate chain associated with the provided hash.
+	// Get returns the issuance chain associated with the provided hash.
 	Get(ctx context.Context, hash string) ([]byte, error)
 
-	// Set inserts the key-value pair of non-leaf certificate chain.
+	// Set inserts the key-value pair of issuance chain.
 	Set(ctx context.Context, hash string, chain []byte) error
 
 	// Purge clears all data in the cache.
