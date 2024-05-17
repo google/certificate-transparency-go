783d6979f19852f7d7123d35508d635e29847035
Roger Ng <rogerng@google.com>
2024-05-17 20:35:40 +0000

    Export `MYSQL_USER_HOST` in ct_functions.sh

--- trillian/integration/ct_functions.sh
+++ trillian/integration/ct_functions.sh
@@ -35,7 +35,8 @@ ct_prep_test() {
 
   # Reset the CT test database
   export MYSQL_HOST="mysql"
-  export MYSQL_ROOT_PASSWORD="bananas"
+  export MYSQL_ROOT_PASSWORD="zaphod"
+  export MYSQL_USER_HOST="%"
   yes | bash "${CT_GO_PATH}/scripts/resetctdb.sh" --verbose
 
   echo "Launching CT personalities"
